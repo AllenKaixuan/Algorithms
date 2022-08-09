a97c77f75e840ec3cb3fd0680a2e1dfd483f51ec
Kaixuan <lxk686868@outlook.com>
2022-08-09 18:21:04 +0800

    bubblesort

---
+++ bubbleSort.cpp
@@ -0,0 +1,57 @@
+/*
+冒泡排序是最简单的排序算法，通过交换相邻元素排序，但是不适用
+于大型数据上，因为其最坏和平均时间复杂度都很高，为O(n^2),即
+使数组已经是有序的，也会一直排序。因此可以当内循环没有交换时
+停止程序。
+*/
+#include <iostream>
+using namespace std;
+void swap(int &a, int &b)
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void bubbleSort(int *arr, int n)
+{
+    for (int i = 0; i < n; i++) // 外循环为选取bubble的次数，同时记录有几个已排好序
+    {
+        for (int j = n - 1; j > i; j--) // 内循环为bubble从后向前冒泡
+        {
+            if (arr[j] < arr[j - 1])
+                swap(arr[j], arr[j - 1]);
+        }
+    }
+}
+
+void bubbleSortOptimized(int *arr, int n)
+{
+    bool swapped;  // 判断是否交换
+    for (int i = 0; i < n; i++)
+    {
+        swapped = 0;
+        for (int j = n - 1; j > i; j--)
+        {
+            if (arr[j] < arr[j - 1])
+            {
+                swap(arr[j], arr[j - 1]);
+                swapped = 1;
+            }
+        }
+        if (!swapped)
+            break;
+    }
+}
+
+int main()
+{
+    int a[5] = {3, 1, 2, 4, 6};
+    //bubbleSort(a, 5);
+    bubbleSortOptimized(a, 5);
+    for (int i = 0; i < 5; i++)
+    {
+        cout << a[i] << " ";
+    }
+}
