ecdf171c0a6f7d25507c574b85d3bec1706c0e5b
Kaixuan <lxk686868@outlook.com>
2022-08-14 22:31:15 +0800

    graph

--- Graph algorithm/DFS.cpp
+++ Graph algorithm/DFS.cpp
@@ -1,21 +1,32 @@
 /*
 深度优先遍历（Depth First Traverse）
 图的遍历相较于树的遍历，在于有圈，需要用一个数组保存是否visit
+本例使用递归的方法，当然也可以使用栈的结构
 */
 #include <iostream>
 #include <map>
 #include <list>
+#include <stack>
 using namespace std;
 
 class Graph
 {
-public:
     map<int, bool> visited; // bool默认为0
     map<int, list<int>> adj;
+
+public:
+    Graph(){};
+    Graph(const Graph &);
     void addEdge(int v, int w);
     void DFS(int v);
-    void DFSUtil(int v); // 对于非连通图
+    void DFSUtil(int v);  // 对于非连通图
+    void DFSStack(int v); // 使用栈不使用递归
 };
+Graph::Graph(const Graph &G)
+{
+
+    this->adj = G.adj; // 相当于刷新visited数组
+}
 
 void Graph::addEdge(int v, int w)
 {
@@ -33,14 +44,36 @@ void Graph::DFS(int v)
 }
 void Graph::DFSUtil(int v)
 {
-    for(auto adjcent: adj) // 遍历一遍所有节点
-        if(!visited[adjcent.first])
+    for (auto adjcent : adj) // 遍历一遍所有节点
+        if (!visited[adjcent.first])
             DFS(adjcent.first);
+}
+
+void Graph::DFSStack(int v)
+{
+    visited[v] = true;
+    stack<int> s;
+    s.push(v); // 利用先进后出的结构
+    cout << v << " ";
+    while (!s.empty())
+    {
+        int temp = s.top();
+        s.pop();
+        for (auto adjcent : adj[temp])
+        {
+            if (!visited[adjcent])
+            {
+                s.push(adjcent);
+                visited[adjcent] = true;
+                cout << adjcent << " ";
+            }
+        }
+    }
 }
 
 int main()
 {
-    
+
     Graph g;
     g.addEdge(0, 1);
     g.addEdge(0, 9);
@@ -48,12 +81,17 @@ int main()
     g.addEdge(2, 0);
     g.addEdge(2, 3);
     g.addEdge(9, 3);
- 
+
     cout << "Following is Depth First Traversal"
             " (starting from vertex 2) \n";
-    //g.DFS(2);
-    g.DFSUtil(2); // 遍历顺序可能与DFS不一样
-
- 
+    g.DFS(2);
+    cout << endl
+         << "非连通图也可以使用：";
+    Graph h = g; // 拷贝构造函数，刷新visted数组
+    h.DFSUtil(2); // 遍历顺序可能与DFS不一样
+    cout << endl
+         << "使用栈： ";
+    Graph l = g;
+    l.DFSStack(2);
     return 0;
 }
